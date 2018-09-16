a9c0140c8d3fd1c24b285016624e9716e8c5947c
colevahey <rcvahey01@gmail.com>
2018-09-16 00:18:02 -0400

    Going to tinker with testing to make results more comprehensive

--- src/Testing/main.go
+++ src/Testing/main.go
@@ -38,7 +38,7 @@ func welcome() {
 	fmt.Println("\033[H\033[2JWelcome to J.A.R.V.I.S")
 	fmt.Println("What is it you would like to know?")
 	answer := input()
-	resp, err := http.Get("https://api.wolframalpha.com/v2/query?appid=5KWG7E-HJEU5JGQX6&output=json&includepodid=Result&input=" + strings.Replace(answer, " ", "%20", -1))
+	resp, err := http.Get("https://api.wolframalpha.com/v2/query?appid=5KWG7E-HJEU5JGQX6&output=json&input=" + strings.Replace(answer, " ", "%20", -1))
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -54,7 +54,7 @@ func welcome() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(data.QueryResult.Pods[0].Subpods[0].Plaintext)
+	//fmt.Println(data.QueryResult.Pods[0].Subpods[0].Plaintext)
 }
 
 func main() {
