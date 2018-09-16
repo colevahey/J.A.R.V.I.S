d604d06531039cff6eb9e64f1fd054dc1850d82b
colevahey <rcvahey01@gmail.com>
2018-09-16 00:25:36 -0400

    Small wording change

--- src/JARVIS/main.go
+++ src/JARVIS/main.go
@@ -36,7 +36,7 @@ func input() (text string) {
 
 func welcome() {
 	fmt.Println("\033[H\033[2JWelcome to J.A.R.V.I.S")
-	fmt.Println("What is it you would like to know?")
+	fmt.Println("What would like to know?")
 	answer := input()
 	resp, err := http.Get("https://api.wolframalpha.com/v2/query?appid=5KWG7E-HJEU5JGQX6&output=json&includepodid=Result&input=" + strings.Replace(answer, " ", "%20", -1))
 	if err != nil {
